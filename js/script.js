b7abfaf8dd5b1ed576a14aa229166c8a21e45453
adarshadhungel1 <dadarsha6@gmail.com>
2023-04-03 20:42:44 +0545

    created book object and added a skeleton function for adding data to table in html

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ class Book {
         this.status = status;
         this.remark = remark;
     }
+    addToTable(){
+        return;
+    }
 }
 
 function newData(){
@@ -12,6 +15,8 @@ function newData(){
     bookauthor = document.querySelector("#author").value;
     readstatus= document.querySelector('input[name="status"]:checked').value;
     bookremarks = document.querySelector("#remarks").value;
+
+    new Book(bookname, bookauthor, readstatus, bookremarks).addToTable();
 }
 
 addBook = document.querySelector(".addbook");
