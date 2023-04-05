6b45ffa2b6e8e66d18055e844c02aa6d8e6846ff
adarshadhungel1 <dadarsha6@gmail.com>
2023-04-05 13:13:24 +0545

    added functions to add details on page when buttons are clicked

--- js/script.js
+++ js/script.js
@@ -6,8 +6,31 @@ class Book {
         this.remark = remark;
     }
     addToTable(){
-        return;
+        this.tableData = document.querySelectorAll('tr');
+        this.tableData.innerHTML += `<tr>
+                            <td> ${this.name} </td>
+                            <td> ${this.author} </td>
+                            <td> ${this.status} </td>
+                            <td> ${this.remark} </td>
+                           </tr>`;
+        console.log(this.remark);
     }
+}
+
+function reveal(){
+    target = document.querySelector(".reveal");
+    target.innerHTML = `<div class="tail">
+            <h3>New Book Info</h3>
+            <div class="name"> <label for="name"> Name: </label> <input type="text" name="name" id="name"> </div>
+            <div class="author"> <label for="author">Author: </label> <input type="text" name="author" id="author"> </div>
+            <div class="yn"> <label for="status"> Have you read it yet?: </label> <input type="radio" name="status" value="yes"> Yes <input type="radio" name="status" value="no" checked> No </div>
+            <div class="remarks"><label for="remarks"> Remarks: </label><input type="text" name="remarks" id="remarks"></div>
+            <button class="addbook">Add</button>
+            </div>`;
+    
+    addBook = document.querySelector(".addbook");
+    addBook.addEventListener('click',newData);
+    return;
 }
 
 function newData(){
@@ -19,5 +42,5 @@ function newData(){
     new Book(bookname, bookauthor, readstatus, bookremarks).addToTable();
 }
 
-addBook = document.querySelector(".addbook");
-addBook.addEventListener('click',newData);
+addNewBook = document.querySelector(".expand");
+addNewBook.addEventListener('click',reveal);
